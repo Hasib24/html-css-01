8952fdac95c7066a0c65e41a6b66921f34300c75
Hasib24 <hasibislam1488@gmail.com>
2022-11-03 09:53:09 +0600

    img compress and remove unecessary files

--- project 1/systematic/js/main.js
+++
@@ -1,8 +0,0 @@
-// Bx Slider
-  $(document).ready(function(){
-    $('.slider_area').bxSlider();
-    $('#main_menu').meanmenu({
-        meanMenuContainer: '#mobile_menu',
-        meanScreenWidth: 991,
-    });
-  });
