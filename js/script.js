971b47728df9463de047b1e1149f5e48cea27ca4
Hasib24 <hasibislam1488@gmail.com>
2022-11-07 00:05:59 +0600

    add responsive toggle menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 console.log("connected");
 
-const navbar = document.querySelector("#navbar");
-
+const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll",()=>{
     navbar.classList.toggle("sticky", window.scrollY > 0)
 })
+
+function toggleMenu(){
+    const toggleMenu =  document.querySelector(".menuToggle");
+    const navigation = document.querySelector(".navUl");
+    navigation.classList.toggle("active")
+    toggleMenu.classList.toggle("active")
+}
