aed9b9a3e0b29dabfd1e659534a64925018e76ec
Hasib24 <hasibislam1488@gmail.com>
2022-11-06 22:01:59 +0600

    add sticky header, inprove style and logo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("connected");
+
+const navbar = document.querySelector("#navbar");
+
+window.addEventListener("scroll",()=>{
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+})
